44ccf685153b9cc07715c53ecd54be20e5b2d621
KSHITIJ CHOUBEY <choubeykshitijanns@gmail.com>
2025-03-27 12:35:13 +0530

    Add files via upload

---
+++ config/variables.py
@@ -0,0 +1,6 @@
+DATA_DIR = "data"
+EXCEL_NAME = "CIS_Red_Hat_Enterprise_Linux_8_Benchmark_v3.0.0.xlsx"
+SUBSHEET_NAME = "Descriptor"
+DB_NAME = "Benchmark.db"
+TABLE_NAME = EXCEL_NAME.split(".")[0]
+JSON_FILE = "output.json"
