d65e3395451170c4e667d7bc9b7244a8d3f72a13
KSHITIJ CHOUBEY <choubeykshitijanns@gmail.com>
2025-03-28 18:28:58 +0530

    Deleting main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-import scripts.xlsx_to_sqlite3
-import scripts.sqlite3_to_json
-from config.variables import EXCEL_NAME, DB_NAME, TABLE_NAME, JSON_FILE, SUBSHEET_NAME
-
-scripts.xlsx_to_sqlite3.xlsx_to_sqlite(EXCEL_NAME, SUBSHEET_NAME, DB_NAME, TABLE_NAME)
-scripts.sqlite3_to_json.sqlite_to_json(DB_NAME, TABLE_NAME, JSON_FILE)
